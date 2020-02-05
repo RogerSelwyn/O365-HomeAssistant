ded30b2627282d225f5204f702449a34773b0d2c
Patrick Toft Steffensen <17211264+PTST@users.noreply.github.com>
2020-02-05 15:03:42 +0100

    Update sensor.py

--- custom_components/o365/sensor.py
+++ custom_components/o365/sensor.py
@@ -127,7 +127,8 @@ class O365QuerySensor(Entity):
         attrs = [get_email_attributes(x) for x in mails]
         attrs.sort(key=itemgetter("received"), reverse=True)
         self._state = len(mails)
-        self._attributes = {"data": attrs, "data_str_repr": json.dumps(attrs)}
+        # self._attributes = {"data": attrs, "data_str_repr": json.dumps(attrs)}
+        self._attributes = {"data": attrs}
 
 
 class O365InboxSensor(Entity):
