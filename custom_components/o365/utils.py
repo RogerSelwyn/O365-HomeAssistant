26f9b3b358e4475db1069c514f6ea8152c98444e
Patrick Toft Steffensen <17211264+PTST@users.noreply.github.com>
2020-02-05 15:05:54 +0100

    Update utils.py

--- custom_components/o365/utils.py
+++ custom_components/o365/utils.py
@@ -1,9 +1,15 @@
 import os
 import json
 import zipfile
+from datetime import datetime
 from pathlib import Path
 from bs4 import BeautifulSoup
 from .const import DEFAULT_CACHE_PATH, SCOPE, CONFIG_BASE_DIR, DATETIME_FORMAT
+from O365.calendar import Attendee
+
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 
 def clean_html(html):
@@ -61,3 +67,57 @@ def get_email_attributes(mail):
         "has_attachments": mail.has_attachments,
         "attachments": [x.name for x in mail.attachments],
     }
+
+
+def add_call_data_to_event(event, event_data):
+    subject = event_data.get("subject")
+    if subject:
+        event.subject = subject
+
+    body = event_data.get("body")
+    if body:
+        event.body = body
+
+    location = event_data.get("location")
+    if location:
+        event.location = location
+
+    categories = event_data.get("categories")
+    if categories:
+        _LOGGER.warn(categories)
+        event.categories = categories
+
+    show_as = event_data.get("show_as")
+    if show_as:
+        event.show_as = show_as
+
+    attendees = event_data.get("attendees")
+    if attendees:
+        event.attendees.clear()
+        event.attendees.add(
+            [
+                Attendee(x["email"], attendee_type=x["type"], event=event)
+                for x in attendees
+            ]
+        )
+
+    start = event_data.get("start")
+    if start:
+        event.start = datetime.strptime(start, DATETIME_FORMAT)
+
+    end = event_data.get("end")
+    if end:
+        event.end = datetime.strptime(end, DATETIME_FORMAT)
+
+    is_all_day = event_data.get("is_all_day")
+    if is_all_day is not None:
+        event.is_all_day = is_all_day
+        if event.is_all_day:
+            event.start = datetime(
+                event.start.year, event.start.month, event.start.day, 0, 0, 0
+            )
+            event.end = datetime(
+                event.end.year, event.end.month, event.end.day, 0, 0, 0
+            )
+
+    return event
