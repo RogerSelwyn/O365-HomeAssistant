636d18827ce6490dd723cb7f73863e519146db18
Patrick Steffensen <17211264+PTST@users.noreply.github.com>
2020-02-17 12:11:51 +0100

    Added support for o365_calendars.yaml file

---
+++ calendar.py
@@ -0,0 +1,299 @@
+import logging
+import copy
+from operator import attrgetter, itemgetter
+from datetime import timedelta, datetime
+from homeassistant.util import Throttle, dt
+from homeassistant.components.calendar import (
+    CalendarEventDevice,
+    calculate_offset,
+    is_offset_reached,
+)
+from .const import (
+    CONF_NAME,
+    CONF_HOURS_FORWARD_TO_GET,
+    CONF_HOURS_BACKWARD_TO_GET,
+    CALENDAR_SERVICE_CREATE_SCHEMA,
+    CALENDAR_SERVICE_MODIFY_SCHEMA,
+    CALENDAR_SERVICE_REMOVE_SCHEMA,
+    CALENDAR_SERVICE_RESPOND_SCHEMA,
+    MIN_TIME_BETWEEN_UPDATES,
+    DOMAIN,
+    YAML_CALENDARS,
+    CONF_ENTITIES,
+    CONF_DEVICE_ID,
+    DEFAULT_OFFSET,
+    CONF_TRACK,
+    CONF_SEARCH,
+    CONF_MAX_RESULTS,
+)
+from .utils import (
+    clean_html,
+    add_call_data_to_event,
+    format_event_data,
+    load_calendars,
+    update_calendar_file,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    """Set up the O365 platform."""
+    if discovery_info is None:
+        return
+
+    account = hass.data[DOMAIN]["account"]
+    is_authenticated = account.is_authenticated
+    if not is_authenticated:
+        return False
+
+    calendar_services = CalendarServices(account, True, hass)
+    calendar_services.scan_for_calendars(None)
+
+    calendars = load_calendars(YAML_CALENDARS)
+    devices = []
+
+    for cal_id, calendar in calendars.items():
+        for entity in calendar.get(CONF_ENTITIES):
+            if not entity[CONF_TRACK]:
+                continue
+            cal = O365CalendarEventDevice(account, cal_id, entity)
+            devices.append(cal)
+    add_devices(devices, True)
+
+    hass.services.register(
+        DOMAIN, "modify_calendar_event", calendar_services.modify_calendar_event
+    )
+    hass.services.register(
+        DOMAIN, "create_calendar_event", calendar_services.create_calendar_event
+    )
+    hass.services.register(
+        DOMAIN, "remove_calendar_event", calendar_services.remove_calendar_event
+    )
+    hass.services.register(
+        DOMAIN, "respond_calendar_event", calendar_services.respond_calendar_event
+    )
+    hass.services.register(
+        DOMAIN, "scan_for_calendars", calendar_services.scan_for_calendars
+    )
+
+    return True
+
+
+class O365CalendarEventDevice(CalendarEventDevice):
+    def __init__(self, account, calendar_id, entity):
+        self.entity = entity
+        self.max_results = entity.get(CONF_MAX_RESULTS)
+        self.start_offset = entity.get(CONF_HOURS_BACKWARD_TO_GET)
+        self.end_offset = entity.get(CONF_HOURS_FORWARD_TO_GET)
+        self.search = entity.get(CONF_SEARCH)
+        self.data = O365CalendarData(
+            account, calendar_id, self.search, self.max_results
+        )
+        self._event = None
+        self._name = entity.get(CONF_NAME)
+        self.device_id = entity.get(CONF_DEVICE_ID)
+        self._offset_reached = False
+        self._data_attribute = []
+
+    @property
+    def device_state_attributes(self):
+        return {"offset_reached": self._offset_reached, "data": self._data_attribute}
+
+    @property
+    def event(self):
+        return self._event
+
+    @property
+    def name(self):
+        return self._name
+
+    async def async_get_events(self, hass, start_date, end_date):
+        return await self.data.async_get_events(hass, start_date, end_date)
+
+    async def async_update(self):
+        self.data.update()
+        event = copy.deepcopy(self.data.event)
+        if event is None:
+            self._event = event
+            return
+        event = calculate_offset(event, DEFAULT_OFFSET)
+        self._offset_reached = is_offset_reached(event)
+        events = list(
+            self.data.o365_get_events(
+                datetime.now() + timedelta(hours=self.start_offset),
+                datetime.now() + timedelta(hours=self.end_offset),
+            )
+        )
+        self._data_attribute = [
+            format_event_data(x, self.data.calendar.calendar_id) for x in events
+        ]
+        self._data_attribute.sort(key=itemgetter("start"))
+        self._event = event
+
+
+class O365CalendarData:
+    def __init__(self, account, calendar_id, search=None, limit=999):
+        self.account = account
+        self.calendar_id = calendar_id
+        self.limit = limit
+        self.schedule = self.account.schedule()
+        self.calendar = self.schedule.get_calendar(calendar_id=self.calendar_id)
+        self.search = search
+        self.event = None
+
+    def o365_get_events(self, start_date, end_date):
+        query = self.calendar.new_query("start").greater_equal(start_date)
+        query.chain("and").on_attribute("end").less_equal(end_date)
+        if self.search is not None:
+            query.chain("and").on_attribute("subject").contains(self.search)
+        return self.calendar.get_events(
+            limit=self.limit, query=query, include_recurring=True
+        )
+
+    async def async_get_events(self, hass, start_date, end_date):
+        vevent_list = list(
+            await hass.async_add_job(self.o365_get_events, start_date, end_date)
+        )
+        vevent_list.sort(key=attrgetter("start"))
+        event_list = []
+        for event in vevent_list:
+            data = format_event_data(event, self.calendar.calendar_id)
+            data["start"] = self.get_hass_date(data["start"])
+            data["end"] = self.get_hass_date(data["end"])
+            event_list.append(data)
+
+        return event_list
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update(self):
+        results = self.o365_get_events(
+            dt.start_of_local_day(), dt.start_of_local_day() + timedelta(days=1)
+        )
+        results = list(results)
+        results.sort(key=lambda x: self.to_datetime(x.start))
+
+        vevent = next((event for event in results if not self.is_over(event)), None,)
+
+        if vevent is None:
+            _LOGGER.debug(
+                "No matching event found in the %d results for %s",
+                len(results),
+                self.calendar.name,
+            )
+            self.event = None
+            return
+
+        self.event = {
+            "summary": vevent.subject,
+            "start": self.get_hass_date(vevent.start),
+            "end": self.get_hass_date(self.get_end_date(vevent)),
+            "location": vevent.location,
+            "description": clean_html(vevent.body),
+        }
+
+    @staticmethod
+    def is_all_day(vevent):
+        return vevent.is_all_day
+
+    @staticmethod
+    def is_over(vevent):
+        return dt.now() >= O365CalendarData.to_datetime(
+            O365CalendarData.get_end_date(vevent)
+        )
+
+    @staticmethod
+    def get_hass_date(obj):
+        if isinstance(obj, datetime):
+            return {"dateTime": obj.isoformat()}
+
+        return {"date": obj.isoformat()}
+
+    @staticmethod
+    def to_datetime(obj):
+        if isinstance(obj, datetime):
+            if obj.tzinfo is None:
+                return obj.replace(tzinfo=dt.DEFAULT_TIME_ZONE)
+            return obj
+        return dt.as_local(dt.dt.datetime.combine(obj, dt.dt.time.min))
+
+    @staticmethod
+    def get_end_date(obj):
+        if hasattr(obj, "end"):
+            enddate = obj.end
+
+        elif hasattr(obj, "duration"):
+            enddate = obj.start + obj.duration.value
+
+        else:
+            enddate = obj.start + timedelta(days=1)
+
+        return enddate
+
+
+class CalendarServices:
+    def __init__(self, account, track_new_found_calendars, hass):
+        self.account = account
+        self.schedule = self.account.schedule()
+        self.track_new_found_calendars = track_new_found_calendars
+        self._hass = hass
+
+    def modify_calendar_event(self, call):
+        event_data = call.data
+        CALENDAR_SERVICE_MODIFY_SCHEMA({k: v for k, v in event_data.items()})
+        calendar = self.schedule.get_calendar(calendar_id=event_data.get("calendar_id"))
+        event = calendar.get_event(event_data["event_id"])
+        event = add_call_data_to_event(event, call.data)
+        event.save()
+        return
+
+    def create_calendar_event(self, call):
+        event_data = call.data
+        CALENDAR_SERVICE_CREATE_SCHEMA({k: v for k, v in event_data.items()})
+        calendar = self.schedule.get_calendar(calendar_id=event_data.get("calendar_id"))
+        event = calendar.new_event()
+        event = add_call_data_to_event(event, call.data)
+        event.save()
+        return
+
+    def remove_calendar_event(self, call):
+        event_data = call.data
+        CALENDAR_SERVICE_REMOVE_SCHEMA({k: v for k, v in event_data.items()})
+        calendar = self.schedule.get_calendar(calendar_id=event_data.get("calendar_id"))
+        event = calendar.get_event(event_data["event_id"])
+        event.delete()
+        return
+
+    def respond_calendar_event(self, call):
+        event_data = call.data
+        CALENDAR_SERVICE_RESPOND_SCHEMA({k: v for k, v in event_data.items()})
+        calendar = self.schedule.get_calendar(calendar_id=event_data.get("calendar_id"))
+        event = calendar.get_event(event_data["event_id"])
+        response = event_data.get("response")
+
+        responses = ["accept", "tentative", "decline"]
+        if response is None:
+            raise ValueError("response not set")
+        if response.lower() not in responses:
+            raise ValueError(f"response must be one of {', '.join(responses)}")
+
+        send_response = event_data.get("send_response", True)
+        if response.lower() == "accept":
+            event.accept_event(event_data.get("message"), send_response=send_response)
+
+        elif response.lower() == "tentative":
+            event.accept_event(
+                event_data.get("message"), tentatively=True, send_response=send_response
+            )
+
+        elif response.lower() == "decline":
+            event.decline_event(event_data.get("message"), send_response=send_response)
+
+        return
+
+    def scan_for_calendars(self, call):
+        """Scan for new calendars."""
+        calendars = self.schedule.list_calendars()
+        for calendar in calendars:
+            track = self.track_new_found_calendars
+            update_calendar_file(YAML_CALENDARS, calendar, self._hass, track)
---
+++ const.py
@@ -0,0 +1,247 @@
+from enum import Enum
+from datetime import timedelta
+from homeassistant.const import CONF_NAME
+from homeassistant.components.notify import (
+    ATTR_DATA,
+    ATTR_TARGET,
+    ATTR_TITLE,
+    ATTR_MESSAGE,
+)
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+
+from homeassistant.config import get_default_config_dir
+
+from O365 import FileSystemTokenBackend
+from O365.calendar import AttendeeType, EventSensitivity, EventShowAs
+
+
+class EventResponse(Enum):
+    Accept = "accept"
+    Tentative = "tentative"
+    Decline = "decline"
+
+
+ATTR_ATTACHMENTS = "attachments"
+ATTR_ATTENDEES = "attendees"
+ATTR_BODY = "body"
+ATTR_CALENDAR_ID = "calendar_id"
+ATTR_CATEGORIES = "categories"
+ATTR_EMAIL = "email"
+ATTR_END = "end"
+ATTR_EVENT_ID = "event_id"
+ATTR_IS_ALL_DAY = "is_all_day"
+ATTR_LOCATION = "location"
+ATTR_MESSAGE_IS_HTML = "message_is_html"
+ATTR_PHOTOS = "photos"
+ATTR_RESPONSE = "response"
+ATTR_SEND_RESPONSE = "send_response"
+ATTR_SENSITIVITY = "sensitivity"
+ATTR_SHOW_AS = "show_as"
+ATTR_START = "start"
+ATTR_SUBJECT = "subject"
+ATTR_TYPE = "type"
+ATTR_ZIP_ATTACHMENTS = "zip_attachments"
+ATTR_ZIP_NAME = "zip_name"
+AUTH_CALLBACK_NAME = "api:o365"
+AUTH_CALLBACK_PATH = "/api/o365"
+AUTH_CALLBACK_PATH_ALT = "https://login.microsoftonline.com/common/oauth2/nativeclient"
+CALENDAR_DOMAIN = "calendar"
+CALENDAR_ENTITY_ID_FORMAT = CALENDAR_DOMAIN + ".o365_{}"
+CONF_ALIASES = "aliases"
+CONF_ALT_CONFIG = "alt_auth_flow"
+CONF_CACHE_PATH = "cache_path"
+CONF_CALENDAR_NAME = "calendar_name"
+CONF_CALENDARS = "calendars"
+CONF_CLIENT_ID = "client_id"
+CONF_CLIENT_SECRET = "client_secret"
+CONF_DEVICE_ID = "device_id"
+CONF_EMAIL_SENSORS = "email_sensor"
+CONF_IGNORE_AVAILABILITY = "ignore_availability"
+CONF_OFFSET = "offset"
+CONF_SEARCH = "search"
+CONF_TRACK = "track"
+CONF_MAX_RESULTS = "max_results"
+CONF_CAL_ID = "cal_id"
+CONF_ENTITIES = "entities"
+
+CONF_HAS_ATTACHMENT = "has_attachment"
+CONF_HOURS_BACKWARD_TO_GET = "start_offset"
+CONF_HOURS_FORWARD_TO_GET = "end_offset"
+CONF_IS_UNREAD = "is_unread"
+CONF_MAIL_FOLDER = "folder"
+CONF_MAIL_FROM = "from"
+CONF_MAX_ITEMS = "max_items"
+CONF_QUERY_SENSORS = "query_sensors"
+CONF_SUBJECT_CONTAINS = "subject_contains"
+CONF_SUBJECT_IS = "subject_is"
+CONF_TRACK_NEW = "track_new_calendar"
+CONFIG_BASE_DIR = get_default_config_dir()
+CONFIGURATOR_DESCRIPTION = (
+    "To link your O365 account, click the link, login, and authorize:"
+)
+CONFIGURATOR_LINK_NAME = "Link O365 account"
+CONFIGURATOR_SUBMIT_CAPTION = "I authorized successfully"
+DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
+DEFAULT_CACHE_PATH = ".O365-token-cache"
+DEFAULT_HOURS_BACKWARD_TO_GET = 0
+DEFAULT_HOURS_FORWARD_TO_GET = 24
+DEFAULT_NAME = "O365"
+DOMAIN = "o365"
+ICON = "mdi:office"
+MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=15)
+DEFAULT_OFFSET = "!!"
+SCOPE = [
+    "offline_access",
+    "User.Read",
+    "Calendars.ReadWrite",
+    "Calendars.ReadWrite.Shared",
+    "Mail.ReadWrite",
+    "Mail.ReadWrite.Shared",
+    "Mail.Send",
+    "Mail.Send.Shared",
+]
+TOKEN_BACKEND = FileSystemTokenBackend(
+    token_path=DEFAULT_CACHE_PATH, token_filename="o365.token"
+)
+YAML_CALENDARS = f"{DOMAIN}_calendars.yaml"
+
+CALENDAR_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_CALENDAR_NAME): cv.string,
+        vol.Optional(CONF_NAME): cv.string,
+        vol.Optional(CONF_HOURS_FORWARD_TO_GET, default=24): int,
+        vol.Optional(CONF_HOURS_BACKWARD_TO_GET, default=0): int,
+    }
+)
+EMAIL_SENSOR = vol.Schema(
+    {
+        vol.Required(CONF_NAME): cv.string,
+        vol.Optional(CONF_MAIL_FOLDER): cv.string,
+        vol.Optional(CONF_MAX_ITEMS, default=5): int,
+        vol.Optional(CONF_IS_UNREAD): bool,
+    }
+)
+QUERY_SENSOR = vol.Schema(
+    {
+        vol.Required(CONF_NAME): cv.string,
+        vol.Optional(CONF_MAIL_FOLDER): cv.string,
+        vol.Optional(CONF_MAIL_FROM): cv.string,
+        vol.Optional(CONF_MAX_ITEMS, default=5): int,
+        vol.Optional(CONF_HAS_ATTACHMENT): bool,
+        vol.Optional(CONF_IS_UNREAD): bool,
+        vol.Exclusive(CONF_SUBJECT_CONTAINS, "subject_*"): cv.string,
+        vol.Exclusive(CONF_SUBJECT_IS, "subject_*"): cv.string,
+    }
+)
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(CONF_CLIENT_ID): cv.string,
+                vol.Required(CONF_CLIENT_SECRET): cv.string,
+                vol.Optional(CONF_TRACK_NEW, default=True): bool,
+                vol.Optional(CONF_ALT_CONFIG, default=False): bool,
+                vol.Optional(CONF_CALENDARS, default=[]): [CALENDAR_SCHEMA],
+                vol.Optional(CONF_EMAIL_SENSORS): [EMAIL_SENSOR],
+                vol.Optional(CONF_QUERY_SENSORS): [QUERY_SENSOR],
+            },
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+NOTIFY_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Optional(ATTR_MESSAGE_IS_HTML, default=False): bool,
+        vol.Optional(ATTR_TARGET): cv.string,
+        vol.Optional(ATTR_ZIP_ATTACHMENTS, default=False): bool,
+        vol.Optional(ATTR_ZIP_NAME): cv.string,
+        vol.Optional(ATTR_PHOTOS, default=[]): [cv.string],
+        vol.Optional(ATTR_ATTACHMENTS, default=[]): [cv.string],
+    }
+)
+
+NOTIFY_BASE_SCHEMA = vol.Schema(
+    {
+        vol.Optional(ATTR_TARGET, default=[]): [cv.string],
+        vol.Optional(ATTR_TITLE, default=""): cv.string,
+        vol.Optional(ATTR_DATA): NOTIFY_DATA_SCHEMA,
+    }
+)
+
+CALENDAR_SERVICE_RESPOND_SCHEMA = vol.Schema(
+    {
+        vol.Required(ATTR_EVENT_ID): cv.string,
+        vol.Required(ATTR_CALENDAR_ID): cv.string,
+        vol.Optional(ATTR_RESPONSE, None): cv.enum(EventResponse),
+        vol.Optional(ATTR_SEND_RESPONSE, True): bool,
+        vol.Optional(ATTR_MESSAGE, None): cv.string,
+    }
+)
+
+ATTENDEE_SCHEMA = vol.Schema(
+    {
+        vol.Required(ATTR_EMAIL): cv.string,
+        vol.Required(ATTR_TYPE): cv.enum(AttendeeType),
+    }
+)
+
+CALENDAR_SERVICE_CREATE_SCHEMA = vol.Schema(
+    {
+        vol.Required(ATTR_CALENDAR_ID): cv.string,
+        vol.Required(ATTR_START): cv.datetime,
+        vol.Required(ATTR_END): cv.datetime,
+        vol.Required(ATTR_SUBJECT): cv.string,
+        vol.Optional(ATTR_BODY): cv.string,
+        vol.Optional(ATTR_LOCATION): cv.string,
+        vol.Optional(ATTR_CATEGORIES): [cv.string],
+        vol.Optional(ATTR_SENSITIVITY): cv.enum(EventSensitivity),
+        vol.Optional(ATTR_SHOW_AS): cv.enum(EventShowAs),
+        vol.Optional(ATTR_IS_ALL_DAY): bool,
+        vol.Optional(ATTR_ATTENDEES): [ATTENDEE_SCHEMA],
+    }
+)
+
+CALENDAR_SERVICE_MODIFY_SCHEMA = vol.Schema(
+    {
+        vol.Required(ATTR_EVENT_ID): cv.string,
+        vol.Required(ATTR_CALENDAR_ID): cv.string,
+        vol.Optional(ATTR_START): cv.datetime,
+        vol.Optional(ATTR_END): cv.datetime,
+        vol.Required(ATTR_SUBJECT): cv.string,
+        vol.Optional(ATTR_BODY): cv.string,
+        vol.Optional(ATTR_LOCATION): cv.string,
+        vol.Optional(ATTR_CATEGORIES): [cv.string],
+        vol.Optional(ATTR_SENSITIVITY): cv.enum(EventSensitivity),
+        vol.Optional(ATTR_SHOW_AS): cv.enum(EventShowAs),
+        vol.Optional(ATTR_IS_ALL_DAY): bool,
+        vol.Optional(ATTR_ATTENDEES): [ATTENDEE_SCHEMA],
+    }
+)
+
+
+CALENDAR_SERVICE_REMOVE_SCHEMA = vol.Schema(
+    {vol.Required(ATTR_EVENT_ID): cv.string, vol.Required(ATTR_CALENDAR_ID): cv.string,}
+)
+
+SINGLE_CALSEARCH_CONFIG = vol.Schema(
+    {
+        vol.Required(CONF_NAME): cv.string,
+        vol.Required(CONF_DEVICE_ID): cv.string,
+        vol.Optional(CONF_HOURS_FORWARD_TO_GET, default=24): int,
+        vol.Optional(CONF_HOURS_BACKWARD_TO_GET, default=0): int,
+        vol.Optional(CONF_SEARCH): cv.string,
+        vol.Optional(CONF_TRACK): cv.boolean,
+        vol.Optional(CONF_MAX_RESULTS): cv.positive_int,
+    }
+)
+
+CALENDAR_DEVICE_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_CAL_ID): cv.string,
+        vol.Required(CONF_ENTITIES, None): vol.All(
+            cv.ensure_list, [SINGLE_CALSEARCH_CONFIG]
+        ),
+    },
+    extra=vol.ALLOW_EXTRA,
+)
