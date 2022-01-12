b917935c6f572f43b2045ff3cff30bcc3514a104
RogerSelwyn <rogers@selwyn.org.uk>
2022-01-12 10:44:29 +0000

    Remove duplicate code

--- custom_components/o365/sensor.py
+++ custom_components/o365/sensor.py
@@ -36,37 +36,39 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 
     unread_sensors = hass.data[DOMAIN].get(CONF_EMAIL_SENSORS, [])
     for conf in unread_sensors:
-        mail_folder = get_mail_folder(hass, account, conf, CONF_EMAIL_SENSORS)
+        mail_folder = _get_mail_folder(hass, account, conf, CONF_EMAIL_SENSORS)
         if mail_folder:
             sensor = O365InboxSensor(conf, mail_folder)
             add_devices([sensor], True)
 
     query_sensors = hass.data[DOMAIN].get(CONF_QUERY_SENSORS, [])
     for conf in query_sensors:
-        mail_folder = get_mail_folder(hass, account, conf, CONF_QUERY_SENSORS)
+        mail_folder = _get_mail_folder(hass, account, conf, CONF_QUERY_SENSORS)
         if mail_folder:
             sensor = O365QuerySensor(conf, mail_folder)
             add_devices([sensor], True)
 
 
-def get_mail_folder(hass, account, conf, sensor_type):
+def _get_mail_folder(hass, account, conf, sensor_type):
     """Get the configured folder."""
     mailbox = account.mailbox()
     mail_folder = None
     mail_folder_conf = conf.get(CONF_MAIL_FOLDER)
     if mail_folder_conf:
         for i, folder in enumerate(mail_folder_conf.split("/")):
-            _LOGGER.debug(f"Processing folder - {folder}")
+            _LOGGER.debug(f"Processing folder - {folder} - from {sensor_type} config entry - {mail_folder_conf} ")
             if i == 0:
                 mail_folder = mailbox.get_folder(folder_name=folder)
             else:
                 mail_folder = mail_folder.get_folder(folder_name=folder)
 
             if not mail_folder:
-                _LOGGER.error(f"Folder - {folder} - not found from {sensor_type} config entry- {mail_folder_conf}")
+                _LOGGER.error(
+                    f"Folder - {folder} - not found from {sensor_type} config entry - {mail_folder_conf} - entity not created"
+                )
                 return None
 
-            _LOGGER.debug(f"Got folder id - {mail_folder.folder_id}")
+            # _LOGGER.debug(f"Got folder id - {mail_folder.folder_id}")
 
     else:
         mail_folder = mailbox.inbox_folder()
@@ -74,15 +76,49 @@ def get_mail_folder(hass, account, conf, sensor_type):
     return mail_folder
 
 
-class O365QuerySensor(Entity):
+class O365Sensor:
+    """O365 generic Sensor class."""
+
+    def __init__(self, conf, mail_folder):
+        """Initialise the O365 Sensor."""
+        self.mail_folder = mail_folder
+        self._name = conf.get(CONF_NAME)
+        self.max_items = conf.get(CONF_MAX_ITEMS, 5)
+        self._state = 0
+        self._attributes = {}
+
+    @property
+    def name(self):
+        """Name property."""
+        return self._name
+
+    @property
+    def state(self):
+        """State property."""
+        return self._state
+
+    @property
+    def extra_state_attributes(self):
+        """Device state attributes."""
+        return self._attributes
+
+    def update(self):
+        """Update code."""
+        mails = list(self.mail_folder.get_messages(limit=self.max_items, query=self.query, download_attachments=True))
+        attrs = [get_email_attributes(x) for x in mails]
+        attrs.sort(key=itemgetter("received"), reverse=True)
+        self._state = len(mails)
+        # self._attributes = {"data": attrs, "data_str_repr": json.dumps(attrs)}
+        self._attributes = {"data": attrs}
+
+
+class O365QuerySensor(O365Sensor, Entity):
     """O365 Query sensor processing."""
 
     def __init__(self, conf, mail_folder):
         """Initialise the O365 Query."""
-        self.mail_folder = mail_folder
-        self._name = conf.get(CONF_NAME)
+        super().__init__(conf, mail_folder)
 
-        self.max_items = conf.get(CONF_MAX_ITEMS, 5)
         self.subject_contains = conf.get(CONF_SUBJECT_CONTAINS)
         self.subject_is = conf.get(CONF_SUBJECT_IS)
         self.has_attachment = conf.get(CONF_HAS_ATTACHMENT)
@@ -108,33 +144,7 @@ class O365QuerySensor(Entity):
         self._add_to_query("equals", "IsRead", not self.is_unread, self.is_unread)
         self._add_to_query("equals", "importance", self.importance)
 
-        _LOGGER.debug(self.query)
-        self._state = None
-        self._attributes = {}
-
-    @property
-    def name(self):
-        """Name property."""
-        return self._name
-
-    @property
-    def state(self):
-        """State property."""
-        return self._state
-
-    @property
-    def extra_state_attributes(self):
-        """Device state attributes."""
-        return self._attributes
-
-    def update(self):
-        """Update code."""
-        mails = list(self.mail_folder.get_messages(limit=self.max_items, query=self.query, download_attachments=True))
-        attrs = [get_email_attributes(x) for x in mails]
-        attrs.sort(key=itemgetter("received"), reverse=True)
-        self._state = len(mails)
-        # self._attributes = {"data": attrs, "data_str_repr": json.dumps(attrs)}
-        self._attributes = {"data": attrs}
+        # _LOGGER.debug(self.query)
 
     def _add_to_query(self, type, attribute_name, attribute_value, check_value=True):
         if attribute_value is None or check_value is None:
@@ -148,46 +158,16 @@ class O365QuerySensor(Entity):
             self.query.chain("and").on_attribute(attribute_name).equals(attribute_value)
 
 
-class O365InboxSensor(Entity):
+class O365InboxSensor(O365Sensor, Entity):
     """O365 Inboox processing."""
 
     def __init__(self, conf, mail_folder):
         """Initialise the O365 Inbox."""
-        self.mail_folder = mail_folder
-        self.max_items = conf.get(CONF_MAX_ITEMS, 5)
+        super().__init__(conf, mail_folder)
+
         self.is_unread = conf.get(CONF_IS_UNREAD)
 
-        self._name = conf.get(CONF_NAME)
-        self._state = 0
-        self._attributes = {}
         self.query = None
         if self.is_unread is not None:
-            if not self.query:
-                self.query = self.mail_folder.new_query()
-            else:
-                self.query.chain("and")
-            self.query.on_attribute("IsRead").equals(not self.is_unread)
-
-    @property
-    def name(self):
-        """Name property."""
-        return self._name
-
-    @property
-    def state(self):
-        """State property."""
-        return self._state
-
-    @property
-    def extra_state_attributes(self):
-        """Device state attributes."""
-        return self._attributes
-
-    def update(self):
-        """Update code."""
-        mails = list(self.mail_folder.get_messages(limit=self.max_items, query=self.query, download_attachments=True))
-        attrs = [get_email_attributes(x) for x in mails]
-        attrs.sort(key=itemgetter("received"), reverse=True)
-        self._state = len(mails)
-        # self._attributes = {"data": attrs, "data_str_repr": json.dumps(attrs)}
-        self._attributes = {"data": attrs}
+            self.query = self.mail_folder.new_query()
+            self.query.chain("and").on_attribute("IsRead").equals(not self.is_unread)
