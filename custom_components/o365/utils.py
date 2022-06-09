8f445edfaf7edec48e887ca5f44d730e6525b133
RogerSelwyn <rogers@selwyn.org.uk>
2022-06-09 17:48:04 +0100

    Return line breaks where available

--- custom_components/o365/utils.py
+++ custom_components/o365/utils.py
@@ -55,7 +55,17 @@ def clean_html(html):
     """Clean the HTML."""
     soup = BeautifulSoup(html, features="html.parser")
     if body := soup.find("body"):
-        return body.get_text(" ", strip=True)
+        # get text
+        text = body.get_text()
+
+        # break into lines and remove leading and trailing space on each
+        lines = (line.strip() for line in text.splitlines())
+        # break multi-headlines into a line each
+        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+        # drop blank lines
+        text = "\n".join(chunk for chunk in chunks if chunk)
+        text = text.replace("\xa0", " ")
+        return text
 
     return html
 
