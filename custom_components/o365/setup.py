2b23ee5313331fa9af20630eed93ae67183de2cb
RogerSelwyn <rogers@selwyn.org.uk>
2024-05-09 14:24:29 +0100

    maint: Parallel run setup of sensors and mail

--- custom_components/o365/setup.py
+++ custom_components/o365/setup.py
@@ -1,5 +1,8 @@
 """Do configuration setup."""
 
+import asyncio
+import logging
+
 from homeassistant.const import CONF_ENABLED
 from homeassistant.helpers import discovery
 
@@ -24,6 +27,8 @@ from .const import (
     DOMAIN,
 )
 from .coordinator import O365EmailCordinator, O365SensorCordinator
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def do_setup(hass, config, account, account_name, conf_type, perms):
@@ -55,18 +60,40 @@ async def do_setup(hass, config, account, account_name, conf_type, perms):
         hass.data[DOMAIN] = {}
     hass.data[DOMAIN][account_name] = account_config
 
-    sensor_coordinator = O365SensorCordinator(hass, account_config)
-    sensor_keys = await sensor_coordinator.async_setup_entries()
-    await sensor_coordinator.async_config_entry_first_refresh()
-    email_coordinator = O365EmailCordinator(hass, account_config)
-    email_keys = await email_coordinator.async_setup_entries()
-    await email_coordinator.async_config_entry_first_refresh()
+    async with asyncio.TaskGroup() as group:
+        sensors_result = group.create_task(_async_sensor_setup(hass, account_config))
+        email_result = group.create_task(_async_email_setup(hass, account_config))
+
+    sensor_keys = sensors_result.result()["keys"]
+    sensor_coordinator = sensors_result.result()["coordinator"]
+    email_keys = email_result.result()["keys"]
+    email_coordinator = email_result.result()["coordinator"]
     hass.data[DOMAIN][account_name][CONF_KEYS_SENSORS] = sensor_keys
     hass.data[DOMAIN][account_name][CONF_COORDINATOR_SENSORS] = sensor_coordinator
     hass.data[DOMAIN][account_name][CONF_KEYS_EMAIL] = email_keys
     hass.data[DOMAIN][account_name][CONF_COORDINATOR_EMAIL] = email_coordinator
 
     _load_platforms(hass, account_name, config, account_config)
+
+
+async def _async_sensor_setup(hass, account_config):
+    _LOGGER.debug("Sensor setup - start")
+    sensor_coordinator = O365SensorCordinator(hass, account_config)
+    sensor_keys = await sensor_coordinator.async_setup_entries()
+    if sensor_keys:
+        await sensor_coordinator.async_config_entry_first_refresh()
+    _LOGGER.debug("Sensor setup - finish")
+    return {"coordinator": sensor_coordinator, "keys": sensor_keys}
+
+
+async def _async_email_setup(hass, account_config):
+    _LOGGER.debug("Email setup - start")
+    email_coordinator = O365EmailCordinator(hass, account_config)
+    email_keys = await email_coordinator.async_setup_entries()
+    if email_keys:
+        await email_coordinator.async_config_entry_first_refresh()
+    _LOGGER.debug("Email setup - finish")
+    return {"coordinator": email_coordinator, "keys": email_keys}
 
 
 def _load_platforms(hass, account_name, config, account_config):
